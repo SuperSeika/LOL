148624e6ed6ebf4ac5e497e315f51c3029c9fc5b
SuperSeika <elisei.balaenkov@gmail.com>
2020-12-13 18:36:54 +0700

    викторина

--- src/MAIN.kt
+++ src/MAIN.kt
@@ -1,11 +1,50 @@
 fun main()
 {
-    var p = 2
-    var o = 2
-    var i = 2
-    println(i)
-    println(o)
-    println(p)
+    //Приветствие игроку и правила игры
+    println("Добро пожаловать....")
+    println("Правила победы....")
+    println("Правила поражения....")
+
+
+    //Начальные параметры
+    var points = 0
+    var life = 2
+    var answer = 0 //Пустой ответ
+
+
+    //Список вопросов
+    var question1 = ""
+    var question2 = ""
+    var question3 = ""
+    var question4 = ""
+    var question5 = ""
+    var question6 = ""
+    var question7 = ""
+    var question8 = ""
+    var question9 = ""
+    var question10 = ""
+
+
+    //Структура вопроса
+    println(question1) // Задается вопрос
+    answer = readLine()!!.toInt() // Ожидается ответ
+    /*if () или when()*/
+
+    when(answer) //Условия правильного ответа
+    {
+        1 -> {println("Сообщение"); points++} //Правильный ответ
+        2 -> {println("Сообщение"); life--} //Неравильный ответ
+        3 -> {println("Сообщение"); life--} //Неправильный ответ
+        4 -> {println("Сообщение"); life--} //Неправильный ответ
+        else -> {println("Сообщение");life--} //Обработка ошибки
+    }
+
+
+
+
+
+
+
 
 
 }
